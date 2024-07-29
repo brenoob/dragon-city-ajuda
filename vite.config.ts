b0212952b85b6420dc0f34206f12fab3846631b1
brenoob <breno_1993@outlook.com>
2024-07-29 05:47:57 -0300

    � chore: atualizar tsconfig e vite.config
    Atualizando a configuração do TypeScript e do Vite para melhorar a resolução de módulos e compatibilidade com arquivos .vue.

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,15 @@
 // vite.config.ts
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-// Estendendo os tipos do Vite
-declare module 'vite' {
-  export interface UserConfig {
-    test?: {
-      globals?: boolean
-      environment?: string
-      include?: string[]
-    }
-  }
-}
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}']
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 })
