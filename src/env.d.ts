f4fd3c0db4384e9e123b07b23a7b73fe4150ef8b
brenoob <breno_1993@outlook.com>
2024-07-29 06:48:22 -0300

    :recycle: refactor: Atualização do env.d.ts para incluir tipos de imagens e remoção do script build-only do package.json

---
+++ src/env.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component  
+  
+  }
+  
+  // Tipos para arquivos de imagem
+  declare module '*.png' {
+    const pngContent: string
+    export default pngContent
+  }
+  
+  declare module '*.jpg' {
+    const jpgContent: string
+    export default jpgContent
+  }
+  
+  // ... similar changes for other image types
+  
+  declare module '*.webp' {
+    const webpContent: string
+    export default webpContent
+  }
